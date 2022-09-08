4c05fd4271fabb030de5a459b884fb229cf16054
stephenChinag <stephenchinag@gmail.com>
2022-09-08 13:55:09 +0100

    http

--- src/components/hooks/use-http.js
+++ src/components/hooks/use-http.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React,{useState} from 'react'
 
  const useHttp=()=> {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+
+  const sendRequest = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        'https://projectone-35744-default-rtdb.firebaseio.com/tasks.json'
+      );
+
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+
+      const loadedTasks = [];
+
+      for (const taskKey in data) {
+        loadedTasks.push({ id: taskKey,
+           text: data[taskKey].text
+           });
+      }
+
+      setTasks(loadedTasks);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  };
   return (
-    <div></div>
+    <div>
+
+    </div>
   )
 }
 
